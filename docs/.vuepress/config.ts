176c468fc1a3fd9a63f6ddd1378def86b5f39f8c
issyugk3 <xwzhou.sgk@gmail.com>
2025-03-20 11:32:54 +0800

    deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/issyugk3.github.io/',
+  base: '/',
   lang: 'en-US',
   locales: {
     '/': {
